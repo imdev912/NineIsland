40098ac01a49cdfe12b9917b4245cb0ed55d21c3
imdev912 <imdev912@gmail.com>
2022-11-14 11:56:31 +0530

    added log list view

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,9 @@ from django.shortcuts import render
 
 
 def list_view(request):
-    return render(request, "blog/list.html")
+    return render(request, "blog/list/list.html")
 
 
 def detail_view(request, slug):
     context = {"slug": slug}
-    return render(request, "blog/detail.html", context)
+    return render(request, "blog/detail/detail.html", context)
